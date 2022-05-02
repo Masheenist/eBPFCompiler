d8391d4a58dd8f20a889157e93af1f811a281a94
EC2 Default User <ec2-user@ip-172-31-6-36.us-west-1.compute.internal>
2022-05-02 04:23:51 +0000

    pulling in object classes

--- src/pyyc/transpiler.py
+++ src/pyyc/transpiler.py
@@ -11,6 +11,75 @@ import sys
 import os
 from ast import *
 
+# class which forms a doubly linked list of nodes, comprised of simple expressions
+# formed from the input ast built from complex expression
+class SimpleExpression():
+	def __init__(self, type, input1, input2, output, prev, next):
+		self.type   = type      # can be AST node type
+		self.input1 = input1    # will be Name of Variable types (Such as AssName, etc..)
+		self.input2 = input2
+		self.output = output    # will hold Value of Variable type data
+		self.prev   = prev
+		self.next   = next
+
+class VariableTracking():
+	names = []
+	def get_name(self, name = None):
+		if name == None:
+			VariableTracking.names.append(str("temp"+str(len(VariableTracking.names))))
+			return "temp"+str(len(VariableTracking.names))
+		else:
+			if name not in VariableTracking.names:
+				VariableTracking.names.append(str(name))
+			return str(name)
+
+	def data():
+		return VariableTracking.names
+
+	def count():
+		return len(VariableTracking.names)
+
+
+# return simple expression currently at the end of the linked list
+def get_last(simple_expression):
+	while simple_expression.prev != None:
+		simple_expression = simple_expression.prev
+	while simple_expression.next != None:
+		simple_expression = simple_expression.next
+	return simple_expression
+
+def get_value(simple_expression, variable_name):
+	returnValue = None
+	negate = False
+	while simple_expression.prev != None:
+		simple_expression = simple_expression.prev
+	while simple_expression != None:
+		if simple_expression.type != "Add":
+			if simple_expression.input1 == variable_name and simple_expression.output != None:
+				returnValue = simple_expression.output
+		simple_expression = simple_expression.next
+	return returnValue
+
+def print_expressions(simple_expression):
+	while simple_expression.prev != None:
+		simple_expression = simple_expression.prev
+	count = 0
+
+	print ('\nSimple Expression Linked List\n- - - - - - - -')
+
+	print ('[count]\t[type]\t\t[input1]\t[input2]\t[output]')
+	while simple_expression != None:
+		if simple_expression.type == "StartList" :
+			print ('[{0}]\t{1}\t{2}\t\t{3}\t\t{4}'.format(count, simple_expression.type, simple_expression.input1, simple_expression.input2, simple_expression.output))
+		elif simple_expression.type == "UnarySub" :
+			print ('[{0}]\t{1}\t{2}\t\t{3}\t\t{4}'.format(count, simple_expression.type, simple_expression.input1, simple_expression.input2, simple_expression.output))
+		else:
+			print ('[{0}]\t{1}\t\t{2}\t\t{3}\t\t{4}'.format(count, simple_expression.type, simple_expression.input1, simple_expression.input2, simple_expression.output))
+		count += 1
+		simple_expression = simple_expression.next
+	print ('- - - - - - - -\n')
+	return
+
 
 # recursively breaking complex expression into simple expressions, pass in
 # ast object and simple expression linked list, pass up simple expression linked
