eb74815d7ecd1e4769b3d192b0862243bf141d81
Rhett <rhett.hanscom@colorado.edu>
2022-05-03 00:01:23 -0600

    def not done

--- src/pyyc/transpiler.py
+++ src/pyyc/transpiler.py
@@ -9,233 +9,29 @@
 
 import sys
 import os
+import re
 from ast import *
-
-# class which forms a doubly linked list of nodes, comprised of simple expressions
-# formed from the input ast built from complex expression
-class SimpleExpression():
-	def __init__(self, type, input1, input2, output, prev, next):
-		self.type   = type      # can be AST node type
-		self.input1 = input1    # will be Name of Variable types (Such as AssName, etc..)
-		self.input2 = input2
-		self.output = output    # will hold Value of Variable type data
-		self.prev   = prev
-		self.next   = next
-
-class VariableTracking():
-	names = []
-	def get_name(self, name = None):
-		if name == None:
-			VariableTracking.names.append(str("temp"+str(len(VariableTracking.names))))
-			return "temp"+str(len(VariableTracking.names))
-		else:
-			if name not in VariableTracking.names:
-				VariableTracking.names.append(str(name))
-			return str(name)
-
-	def data():
-		return VariableTracking.names
-
-	def count():
-		return len(VariableTracking.names)
-
-
-# return simple expression currently at the end of the linked list
-def get_last(simple_expression):
-	while simple_expression.prev != None:
-		simple_expression = simple_expression.prev
-	while simple_expression.next != None:
-		simple_expression = simple_expression.next
-	return simple_expression
-
-def get_value(simple_expression, variable_name):
-	returnValue = None
-	negate = False
-	while simple_expression.prev != None:
-		simple_expression = simple_expression.prev
-	while simple_expression != None:
-		if simple_expression.type != "Add":
-			if simple_expression.input1 == variable_name and simple_expression.output != None:
-				returnValue = simple_expression.output
-		simple_expression = simple_expression.next
-	return returnValue
-
-def print_expressions(simple_expression):
-	while simple_expression.prev != None:
-		simple_expression = simple_expression.prev
-	count = 0
-
-	print ('\nSimple Expression Linked List\n- - - - - - - -')
-
-	print ('[count]\t[type]\t\t[input1]\t[input2]\t[output]')
-	while simple_expression != None:
-		if simple_expression.type == "StartList" :
-			print ('[{0}]\t{1}\t{2}\t\t{3}\t\t{4}'.format(count, simple_expression.type, simple_expression.input1, simple_expression.input2, simple_expression.output))
-		elif simple_expression.type == "UnarySub" :
-			print ('[{0}]\t{1}\t{2}\t\t{3}\t\t{4}'.format(count, simple_expression.type, simple_expression.input1, simple_expression.input2, simple_expression.output))
-		else:
-			print ('[{0}]\t{1}\t\t{2}\t\t{3}\t\t{4}'.format(count, simple_expression.type, simple_expression.input1, simple_expression.input2, simple_expression.output))
-		count += 1
-		simple_expression = simple_expression.next
-	print ('- - - - - - - -\n')
-	return
-
-# recursively breaking complex expression into simple expressions, pass in
-# ast object and simple expression linked list, pass up simple expression linked
-# list only.
-def recursive_flatten(ast, expression_list, VT):
-
-	if isinstance(ast, Module):
-	# 	return recursive_flatten(ast.body, expression_list, VT)
-	#
-	# elif isinstance(ast, Stmt):
-	# 	for node in ast.nodes:
-		for node in ast.body:
-			recursive_flatten(node, expression_list, VT)
-		# recursive_flatten(x, expression_list, VT) for x in ast.nodes
-		prev_expression = get_last(expression_list)
-		current_expression = SimpleExpression("EndList", None, None, None,  \
-		    prev_expression, None)
-		prev_expression.next = current_expression
-		return SimpleExpression("EndList", None, None, None, None, None)
-
-	elif isinstance(ast, Assign):
-		# print 'Assign : [{0}]'.format(ast)
-		input1 = recursive_flatten(ast.expr, expression_list, VT)
-		#assigning INPUT type - have to go back and get it
-		if input1.output == None:
-			prev_expression = get_last(expression_list)
-			# print "Assign previous = type [{0}]\ninput1 [{1}]\ninput2 [{2}]\noutput [{3}]\n".format(prev_expression.type, prev_expression.input1, prev_expression.input2, prev_expression.output)
-			input1 = prev_expression.input1
-		else:
-			input1 = input1.output
-
-		# print 'input1 : [{0}]'.format(ast)
-		output = recursive_flatten(ast.nodes[0], expression_list, VT)
-		# print 'ouput : [{0}]'.format(output.input1)
-		prev_expression = get_last(expression_list)
-		current_expression = SimpleExpression("Assign", output.input1 , None, \
-    		input1, prev_expression, None)
-		prev_expression.next = current_expression
-		return current_expression
-
-	elif isinstance(ast, AssName):
-		# print 'AssName : [{0}]'.format(ast)
-		prev_expression = get_last(expression_list)
-		current_expression = SimpleExpression("AssName", ast.name, None, prev_expression.input1,  \
-		    prev_expression, None)
-		prev_expression.next = current_expression
-		return current_expression
-
-	elif isinstance(ast, Discard):
-		prev_expression = get_last(expression_list)
-		current_expression = SimpleExpression("Discard", ast.name, None, None,  \
-		    prev_expression, None)
-		prev_expression.next = current_expression
-		return current_expression
-
-
-	elif isinstance(ast, Const):
-		prev_expression = get_last(expression_list)
-		current_expression = SimpleExpression("Const", VT.get_name(), None, ast.value, \
-		    prev_expression, None)
-		prev_expression.next = current_expression
-		return current_expression
-
-	elif isinstance(ast, Name):
-		# print 'Name : [{0}]'.format(ast)
-		prev_expression = get_last(expression_list)
-		current_expression = SimpleExpression("Name", VT.get_name(ast.name), None, None, \
-		    prev_expression, None)
-
-		prev_expression.next = current_expression
-		return current_expression
-
-	elif isinstance(ast, Printnl):
-		print_statement = recursive_flatten(ast.nodes[0], expression_list, VT)
-		# print "Print statment = type [{0}]\ninput1 [{1}]\ninput2 [{2}]\noutput [{3}]\n".format(print_statement.type, print_statement.input1, print_statement.input2, print_statement.output)
-		prev_expression = print_statement
-		current_expression = SimpleExpression("Print", print_statement.output if print_statement.output != None else print_statement.input1, None, None, prev_expression, None)
-		prev_expression.next = current_expression
-		return current_expression
-
-	elif isinstance(ast, Add):
-
-		# print 'Add : [{0}]'.format(ast)
-
-		left_add = recursive_flatten(ast.left, expression_list, VT)
-		if left_add == None:
-			left_add = get_last(expression_list)
-
-		# print 'left [{0}]'.format(left_add.type)
-
-		right_add = recursive_flatten(ast.right, expression_list, VT)
-		if right_add == None:
-			right_add = get_last(expression_list)
-		# print 'right [{0}]'.format(right_add.type)
-
-		prev_expression = get_last(expression_list)
-		current_expression = SimpleExpression("Add", left_add.output if left_add.type == 'Add' or left_add.type == 'UnarySub' else left_add.input1, right_add.output if right_add.type == 'Add' or right_add.type == 'UnarySub'  else right_add.input1, \
-		    VT.get_name(), prev_expression, None)
-		prev_expression.next = current_expression
-
-		return current_expression
-
-	elif isinstance(ast, UnarySub):
-		# print 'UnarySub : [{0}]'.format(ast)
-		negate = recursive_flatten(ast.expr, expression_list, VT)
-		prev_expression = get_last(expression_list)
-		current_expression = SimpleExpression("UnarySub", negate.input1, None, \
-		     VT.get_name(), prev_expression, None)
-		prev_expression.next = current_expression
-		return current_expression
-
-	elif isinstance(ast, CallFunc):
-		# print 'CallFunc : [{0}]'.format(ast)
-		if ast.node.name == 'input':
-			prev_expression = get_last(expression_list)
-			current_expression = SimpleExpression("Input", VT.get_name(), None, \
-			    None, prev_expression, None)
-			prev_expression.next = current_expression
-		return current_expression
-
-	else:
-		raise Exception('Error: unrecognized AST node')
-
-#flatten by breaking into series of assignment statements
-def flatten_expression(ast):
-	VT = VariableTracking()
-	expression_list = SimpleExpression("StartList", None, None, None, None, None)
-	recursive_flatten(ast, expression_list, VT)
-	return expression_list
-
 
 
 
 def print_python3_ast(ast, tabs=0):
 	# if an assign is marked as ctx=Load() or Store() we are indifferent.
-
 	if isinstance(ast, Module):
 		# print every entry in module's body
 		print("\t"*tabs + "Module(\n\tbody=[")
 		for entry in ast.body:
 			print_python3_ast(entry, tabs=2)
 		print("\t]\n)")
-
-
-
 	elif isinstance(ast, FunctionDef):
 		# print every argument
 		print("\t"*tabs + "FunctionDef(\n" + "\t"*(tabs+1) + "name=" + ast.name)
 		print("\t"*(tabs+1) + "args=")
 		print_python3_ast(ast.args, tabs=tabs+2)
-
 		# print every entry in funcs's body
 		print("\t"*(tabs+1) + "body=[")
 		for entry in ast.body:
 			print_python3_ast(entry, tabs=tabs+2)
 		print("\t"*(tabs+1)+"],")
-
 		# print decorator_list
 		if len(ast.decorator_list) == 0:
 			print("\t"*(tabs+1) + "decorator_list=[],")
@@ -244,20 +40,14 @@ def print_python3_ast(ast, tabs=0):
 			for entry in ast.decorator_list:
 				print_python3_ast(entry, tabs = tabs+2)
 			print("\t"*(tabs+1) + "],")
-
 		# print returns (I believe this is type stuff? not relevant though)
 		if ast.returns:
 			print("\t"*(tabs+1) + "returns=" + dump(ast.returns))
 		else:
 			print("\t"*(tabs+1) + "returns=None")
-
 		print("\t"*tabs + ")")
-
-
-
 	elif isinstance(ast, Assign):
 		print ("\t"*tabs + "Assign(")
-
 		# print targets
 		if len(ast.targets) == 0:
 			print("\t"*(tabs+1) + "targets=[]")
@@ -266,60 +56,37 @@ def print_python3_ast(ast, tabs=0):
 			for entry in ast.targets:
 				print_python3_ast(entry, tabs = tabs+2)
 			print("\t"*(tabs+1) + "],")
-
 		# print value
 		print("\t"*(tabs+1) + "value=")
 		print_python3_ast(ast.value, tabs=tabs+2)
-
 		print("\t"*tabs + ")")
-
-
-
 	elif isinstance(ast, Expr):
 		print ("\t"*tabs + "Expr(")
-
 		# print value
 		print("\t"*(tabs+1) + "value=")
 		print_python3_ast(ast.value, tabs=tabs+2)
-
 		print("\t"*tabs + ")")
-
-
-
 	elif isinstance(ast, BinOp):
 		print ("\t"*tabs + "BinOp(")
-
 		# print left
 		print("\t"*(tabs+1) + "left=")
 		print_python3_ast(ast.left, tabs=tabs+2)
-
 		# print op
 		print("\t"*(tabs+1) + "op=" + dump(ast.op))
-
 		# print right
 		print("\t"*(tabs+1) + "right=")
 		print_python3_ast(ast.right, tabs=tabs+2)
-
 		print("\t"*tabs + ")")
-
-
-
 	elif isinstance(ast, UnaryOp):
 		print ("\t"*tabs + "UnaryOp(")
-
 		# print op
 		print("\t"*(tabs+1) + "op=" + dump(ast.op))
-
 		# print operand
 		print("\t"*(tabs+1) + "operand=")
 		print_python3_ast(ast.operand, tabs=tabs+2)
 		print("\t"*tabs + ")")
-
-
-
 	elif isinstance(ast, Call):
 		print("\t"*tabs + "Call(")
-
 		# print args
 		if len(ast.args) == 0:
 			print("\t"*(tabs+1) + "args=[]")
@@ -328,7 +95,6 @@ def print_python3_ast(ast, tabs=0):
 			for entry in ast.args:
 				print_python3_ast(entry, tabs = tabs+2)
 			print("\t"*(tabs+1) + "],")
-
 		# print keywords
 		if len(ast.keywords) == 0:
 			print("\t"*(tabs+1) + "keywords=[]")
@@ -337,20 +103,12 @@ def print_python3_ast(ast, tabs=0):
 			for entry in ast.keywords:
 				print_python3_ast(entry, tabs = tabs+2)
 			print("\t"*(tabs+1) + "]")
-
 		print("\t"*tabs + ")")
-
-
-
 	elif isinstance(ast, Name):
 		# has id, and ctx (context, unimportant i think)
 		print("\t"*tabs + dump(ast))
-
-
-
 	elif isinstance(ast, arguments):
 		print("\t"*tabs + "arguments(")
-
 		# print args
 		if len(ast.args) == 0:
 			print("\t"*(tabs+1) + "args=[]")
@@ -359,7 +117,6 @@ def print_python3_ast(ast, tabs=0):
 			for entry in ast.args:
 				print_python3_ast(entry, tabs = tabs+2)
 			print("\t"*(tabs+1) + "]")
-
 		# print vararg
 		if ast.vararg:
 			print("\t"*(tabs+1) + "vararg=" + dump(ast.vararg))
@@ -374,7 +131,6 @@ def print_python3_ast(ast, tabs=0):
 			for entry in ast.kwonlyargs:
 				print_python3_ast(entry, tabs = tabs+2)
 			print("\t"*(tabs+1) + "]")
-
 		# print kw_defaults
 		if len(ast.kw_defaults) == 0:
 			print("\t"*(tabs+1) + "kw_defaults=[]")
@@ -383,13 +139,11 @@ def print_python3_ast(ast, tabs=0):
 			for entry in ast.kw_defaults:
 				print_python3_ast(entry, tabs = tabs+2)
 			print("\t"*(tabs+1) + "]")
-
 		# print kwarg
 		if ast.kwarg:
 			print("\t"*(tabs+1) + "kwarg=" + dump(ast.kwarg))
 		else:
 			print("\t"*(tabs+1) + "kwarg=None")
-
 		# print defaults
 		if len(ast.defaults) == 0:
 			print("\t"*(tabs+1) + "defaults=[]")
@@ -398,36 +152,101 @@ def print_python3_ast(ast, tabs=0):
 			for entry in ast.defaults:
 				print_python3_ast(entry, tabs = tabs+2)
 			print("\t"*(tabs+1) + "]")
-
-
-
 	elif isinstance(ast, Num):
 		# just has n
 		print("\t"*tabs + dump(ast))
-
-
-
 	elif isinstance(ast, Str):
 		# just has s
 		print("\t"*tabs + dump(ast))
-
-
-
 	elif isinstance(ast, arg):
 		# has arg, and annotation (temporary hack if we can't figure out typing BUT if we bail on figuring out typing we need to at least do complicated types like [] {} and BPF maps)
 		print("\t"*tabs + dump(ast))
-
-
-
 	elif isinstance(ast, Return):
 		# just has value
 		print("\t"*tabs + dump(ast))
-
-
-
 	else:
 		print("UNCAUGHT TYPE " + str(type(ast).__name__))
 		print("\t"*tabs + dump(ast))
+
+
+
+def convert_expression(ast, inst_list):
+	if isinstance(ast, Module):
+		for entry in ast.body:
+			convert_expression(entry, inst_list)
+	elif isinstance(ast, FunctionDef):
+		name = ast.name
+		func_args_list = convert_expression(ast.args, inst_list)
+		print("FUNCDEF_NAME: [{0}]".format(name))
+		print("FUNCDEF_ARGS: {0}".format(func_args_list))
+		func_body = []
+		for entry in ast.body:
+			func_body.append(convert_expression(entry, inst_list))
+			print("BODY: [{0}]".format(func_body[-1]))
+		if ast.returns:
+			print("FUNCDEF_RETURNS: [{0}]]".format(ast.returns))
+	elif isinstance(ast, Assign):
+		if len(ast.targets) != 0:
+			target_name = []
+			for entry in ast.targets:
+				target_name.append(convert_expression(entry, inst_list))
+		target_value = convert_expression(ast.value, inst_list)
+
+		return(["ASSIGN", "{} = {}".format(target_name[-1], target_value)])
+	elif isinstance(ast, Expr):
+		if isinstance(ast.value, Call):
+			function_name = ast.value.func.id
+			function_arguments = convert_expression(ast.value, inst_list)
+			print_string = "{0}(".format(function_name)
+			first = True
+			for argu in function_arguments:
+				if not first:
+					print_string += ", {0}".format(arg)
+				else:
+					first = False
+					print_string += argu
+			print_string+=")"
+			return(["CALL", print_string])
+	elif isinstance(ast, BinOp):
+		left_exp = convert_expression(ast.left, inst_list)
+		binop_op = str(dump(ast.op))[:-2]
+		binop_sy  = ''
+		if binop_op == 'Add':
+			binop_sy = '+'
+		right_exp = convert_expression(ast.right, inst_list)
+		return("{} {} {}".format(left_exp, binop_sy, right_exp))
+	elif isinstance(ast, UnaryOp):
+		print("UNARYOP_OP: [{0}]".format(dump(ast.op)))
+		print("UNARYOP_OPERAND: [{0}]".format(dump(ast.operand)))
+	elif isinstance(ast, Call):
+		if len(ast.args) != 0:
+			call_args = []
+			for entry in ast.args:
+				call_args.append(convert_expression(entry, inst_list))
+			return call_args
+	elif isinstance(ast, Name):
+		name_string = str(dump(ast)).split('\'')[1]
+		return name_string
+	elif isinstance(ast, arguments):
+		if len(ast.args) != 0:
+			args_list = []
+			for entry in ast.args:
+				args_list.append(convert_expression(entry, inst_list))
+				# convert_expression(entry, inst_list)
+			return args_list
+	elif isinstance(ast, Num):
+		const_num_list = re.findall(r'\d+', str(dump(ast)))
+		return const_num_list[0]
+	elif isinstance(ast, Str):
+		return str("\"{0}\"".format(str(ast.value)[:-2]))
+	elif isinstance(ast, arg):
+		return_string = str(dump(ast)).split('\'')
+		return return_string[1]
+	elif isinstance(ast, Return):
+		return ["RETURN",ast.value.id]
+	else:
+		print(("UNCAUGHT TYPE " + str(type(ast).__name__)))
+		print(("\t"+ dump(ast)))
 
 
 def main():
@@ -436,14 +255,15 @@ def main():
 		print('\nTest File [{1}]\n- - - - - - - -\n{0}- - - - - - - -'.format(file_text, sys.argv[1]))
 		ast = parse(file_text)#compiler.parse(file_text)
 		# print("ast:[{0}]".format(ast))
-		print_python3_ast(ast)
+		# print_python3_ast(ast)
+		convert_expression(ast, [])
 		# print()
 		#print(dump(ast))
 		#print()
-	print('\nAST\n- - - - - - - -\n{0}\n- - - - - - - -'.format(ast))
-	first_expression = flatten_expression(ast)
-	print_expressions(first_expression)
-	traverse_list(first_expression, sys.argv[1].replace(".py",".s"))
+	print("DONE")
+	# print('\nAST\n- - - - - - - -\n{0}\n- - - - - - - -'.format(ast))
+	# first_expression = flatten_expression(ast)
+	# print_expressions(first_expression)
+	# traverse_list(first_expression, sys.argv[1].replace(".py",".s"))
 
 main()
-
